57f7226d9eaca22c8d3dfe72eda42bcde2db8ee0
shin-dongheon <tlsejdgjs@naver.com>
2023-09-07 16:48:16 +0900

    .

---
+++ mysite02/src/main/java/com/poscodx/mysite/controller/MainController.java
@@ -0,0 +1,20 @@
+package com.poscodx.mysite.controller;
+
+import javax.servlet.*;
+import javax.servlet.http.*;
+import java.io.IOException;
+
+public class MainController extends HttpServlet {
+    private static final long serialVersionUID = 1L;
+
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        request
+                .getRequestDispatcher("/WEB-INF/views/main/index.jsp")
+                .forward(request, response);
+    }
+
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        doGet(request, response);
+    }
+
+}
