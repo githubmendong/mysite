01f9ea2fffea0986fdf4118fbd9f2a07fd1b57e3
shin-dongheon <tlsejdgjs@naver.com>
2023-09-16 23:33:25 +0900

    # Fix done

---
+++ mysite02/src/main/java/com/poscodx/mysite/web/utils/test.java
@@ -0,0 +1,20 @@
+//package com.poscodx.mysite.web.utils;
+//
+//import javax.servlet.*;
+//import javax.servlet.http.*;
+//import javax.servlet.annotation.*;
+//import java.io.IOException;
+//
+//@WebServlet(name = "test", value = "/test")
+//public class test extends HttpServlet {
+//    @Override
+//    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//
+//    }
+//
+//    @Override
+//    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+//
+//    }
+//}
+//
