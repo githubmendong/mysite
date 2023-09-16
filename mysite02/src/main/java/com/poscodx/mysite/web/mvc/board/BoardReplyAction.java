8bcfae292f13381727f2688a1ee2fc75a813fac1
shin-dongheon <tlsejdgjs@naver.com>
2023-09-16 23:33:02 +0900

    # Fix done

--- mysite02/src/main/java/com/poscodx/mysite/web/mvc/board/BoardReplyAction.java
+++ mysite02/src/main/java/com/poscodx/mysite/web/mvc/board/BoardReplyAction.java
@@ -16,12 +16,8 @@ import static com.poscodx.mysite.vo.BoardVo.*;
 
 
 public class BoardReplyAction implements Action {
-
-
     @Override
     public void execute(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-
-
         HttpSession session = request.getSession();
         UserVo authUser = (UserVo) session.getAttribute("authUser");
 
