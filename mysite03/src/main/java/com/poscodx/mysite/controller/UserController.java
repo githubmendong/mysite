badbaa4eb56b336d23cd3059e7ce1c63e08ab38d
shin-dongheon <tlsejdgjs@naver.com>
2023-09-22 11:58:23 +0900

    Validation #2

--- mysite03/src/main/java/com/poscodx/mysite/controller/UserController.java
+++ mysite03/src/main/java/com/poscodx/mysite/controller/UserController.java
@@ -1,8 +1,12 @@
 package com.poscodx.mysite.controller;
 
+import javax.validation.Valid;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 
@@ -23,7 +27,17 @@ public class UserController {
 	}
 
 	@RequestMapping(value="/join", method=RequestMethod.POST)
-	public String join(UserVo userVo) {
+	public String join(@ModelAttribute @Valid UserVo userVo, BindingResult result, Model model) {
+		if(result.hasErrors()) {
+			// List<ObjectError> list = result.getAllErrors();
+			// for(ObjectError error : list) {
+			//	System.out.println(error);
+			// }
+
+			model.addAllAttributes(result.getModel());
+			return "user/join";
+		}
+
 		userService.join(userVo);
 		return "redirect:/user/joinsuccess";
 	}
@@ -37,21 +51,21 @@ public class UserController {
 	public String login() {
 		return "user/login";
 	}
-	
+
 	@Auth
 	@RequestMapping(value="/update", method=RequestMethod.GET)
-	public String update(@AuthUser UserVo authUser, Model model) {		
+	public String update(@AuthUser UserVo authUser, Model model) {
 		UserVo userVo = userService.getUser(authUser.getNo());
 		model.addAttribute("userVo", userVo);
 		return "user/update";
 	}
-	
+
 	@Auth
 	@RequestMapping(value="/update", method=RequestMethod.POST)
 	public String update(@AuthUser UserVo authUser, UserVo userVo) {
 		userVo.setNo(authUser.getNo());
 		userService.update(userVo);
-		
+
 		authUser.setName(userVo.getName());
 		return "redirect:/user/update";
 	}
